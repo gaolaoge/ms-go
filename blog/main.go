2f617dfbf1a095dfa81ab89847c864d93359816f
gaoge <183412808@qq.com>
2023-04-29 14:21:17 +0800

    封装不同请求方式

--- blog/main.go
+++ blog/main.go
@@ -13,9 +13,16 @@ func main() {
 		fmt.Fprintf(writer, "hello world")
 	})
 
+	engine.Get("/hello2", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprintf(writer, "hello world2")
+	})
+
 	v1 := engine.Group("/v1")
-	v1.Add("/name", func(writer http.ResponseWriter, requset *http.Request) {
-		fmt.Fprintf(writer, "gaoge")
+	v1.Get("/name", func(writer http.ResponseWriter, requset *http.Request) {
+		fmt.Fprintf(writer, "get gaoge")
+	})
+	v1.Post("/name", func(writer http.ResponseWriter, requset *http.Request) {
+		fmt.Fprintf(writer, "post gaoge")
 	})
 
 	engine.Run()
--- ms-go/ms.go
+++ ms-go/ms.go
@@ -1,18 +1,31 @@
 package ms_go
 
 import (
+	"fmt"
 	"net/http"
 )
 
 type HandleFunc func(w http.ResponseWriter, r *http.Request)
 
 type routerGroup struct {
-	name          string
-	handleFuncMap map[string]HandleFunc
+	name             string
+	handleFuncMap    map[string]HandleFunc
+	handlerMethodMap map[string][]string
 }
 
 func (rg *routerGroup) Add(name string, handleFunc HandleFunc) {
 	rg.handleFuncMap[name] = handleFunc
+	rg.handlerMethodMap["Any"] = append(rg.handlerMethodMap["Any"], name)
+}
+
+func (rg *routerGroup) Get(name string, handleFunc HandleFunc) {
+	rg.handleFuncMap[name] = handleFunc
+	rg.handlerMethodMap[http.MethodGet] = append(rg.handlerMethodMap[http.MethodGet], name)
+}
+
+func (rg *routerGroup) Post(name string, handleFunc HandleFunc) {
+	rg.handleFuncMap[name] = handleFunc
+	rg.handlerMethodMap[http.MethodPost] = append(rg.handlerMethodMap[http.MethodPost], name)
 }
 
 type router struct {
@@ -22,41 +35,89 @@ type router struct {
 func (r *router) Group(name string) *routerGroup {
 	// TODO 这里需要判断 group 是否已被创建过
 	newGroup := &routerGroup{
-		name:          name,
-		handleFuncMap: make(map[string]HandleFunc),
+		name:             name,
+		handleFuncMap:    make(map[string]HandleFunc),
+		handlerMethodMap: make(map[string][]string),
 	}
 	r.routerGroups = append(r.routerGroups, newGroup)
 	return newGroup
 }
 
 func (r *router) Add(name string, handleFunc HandleFunc) {
-	r.routerGroups[0].handleFuncMap[name] = handleFunc
+	_default := r.routerGroups[0]
+	_default.handleFuncMap[name] = handleFunc
+	_default.handlerMethodMap["Any"] = append(_default.handlerMethodMap["Any"], name)
+}
+
+func (r *router) Get(name string, handleFunc HandleFunc) {
+	_default := r.routerGroups[0]
+	_default.handleFuncMap[name] = handleFunc
+	_default.handlerMethodMap[http.MethodGet] = append(_default.handlerMethodMap[http.MethodGet], name)
+}
+
+func (r *router) Post(name string, handleFunc HandleFunc) {
+	_default := r.routerGroups[0]
+	_default.handleFuncMap[name] = handleFunc
+	_default.handlerMethodMap[http.MethodPost] = append(_default.handlerMethodMap[http.MethodPost], name)
 }
 
 type Engine struct {
 	router
 }
 
-func (e *Engine) Run() {
-	// TODO 尚未添加对"/"的处理，可以在此处建立一些规则
+func (e *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// 1. 遍历 groups 匹配到 URL
+	// 2. 在 methodMap 中匹配 method
+	method := r.Method
 	for _, group := range e.routerGroups {
-		for key, value := range group.handleFuncMap {
-			var path string
+		for name, handle := range group.handleFuncMap {
+			var url string
 			if group.name == "default" {
-				path = key
+				url = name
 			} else {
-				path = group.name + key
+				url = group.name + name
 			}
-			http.HandleFunc(path, value)
+			if r.RequestURI == url {
+				routers, ok := group.handlerMethodMap["Any"]
+				if ok {
+					for _, routerName := range routers {
+						if routerName == name {
+							handle(w, r)
+							return
+						}
+					}
+				}
+				routers, ok = group.handlerMethodMap[method]
+				if ok {
+					for _, routerName := range routers {
+						if routerName == name {
+							handle(w, r)
+							return
+						}
+					}
+				}
+				w.WriteHeader(http.StatusMethodNotAllowed)
+				fmt.Fprintf(w, `%s %s not allowed \n`, r.RequestURI, method)
+				return
+			}
 		}
 	}
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprintf(w, `%s not found \n`, r.RequestURI)
+}
+
+func (e *Engine) Run() {
+	http.Handle("/", e)
 	http.ListenAndServe(":8081", nil)
 }
 
 func New() *Engine {
 	return &Engine{
 		router{
-			routerGroups: []*routerGroup{{"default", make(map[string]HandleFunc)}},
+			routerGroups: []*routerGroup{{
+				name:             "default",
+				handleFuncMap:    make(map[string]HandleFunc),
+				handlerMethodMap: make(map[string][]string)}},
 		},
 	}
 }
